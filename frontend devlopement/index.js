a73c0bb0f5311a8ca368b89181097acf231edbe6
farha-zeenat <farhazeenat10@gmail.com>
2025-09-22 11:22:14 +0530

    working on frontend js

---
+++ frontend devlopement/index.js
@@ -0,0 +1,69 @@
+//console.log("Hello World");
+//var a=5;
+//function test(){
+    //var a=6;
+    //a=a=2;
+    //console.log(a);
+//}
+
+//test();
+
+/*let a=5;
+function test(){
+    let a=5;
+    a=a+5;
+
+}
+test();
+console.log(a);
+
+let a=5;
+let b='5';
+
+console.log(a===b);
+
+types of  data types:
+1.String
+2.Number
+3.BigInt
+4.Boolean
+5.undefined
+6.Null
+7.Symbol
+8.Object 
+
+let a=undefined;
+console.log(typeof a);*/
+
+const arr=[4,5,6,7,8];
+//console.log(a[3]);
+//for(let a of arr){
+  //  console.log(a);
+//}
+//for(let a in arr){
+   // console.log(arr[a]);
+//}
+
+//const person={fname:"Jhone",lane:"Doe",age:"25"};
+//for(let x in person){
+   // console.log(person[x]);
+//}
+
+//function print(a){
+   // console.log(a);
+//}
+
+
+//const print = function (a){
+    //console.log(a);
+//}
+
+//const print=(a)=>{
+    //console.log(a);
+//}
+
+
+arr.forEach ((a)=>{
+    console.log(a);
+
+});
